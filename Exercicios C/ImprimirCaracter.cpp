d07f78f4e8fbdab125227592432c10cb1388416b
Joao Victor Torrezan <jvtorrezan18@gmail.com>
2025-05-27 10:47:24 -0300

    Add files via upload

---
+++ Exercicios C/ImprimirCaracter.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    char c;
+    printf("Digite um caractere : ");
+    scanf("%c", &c);
+    printf("O codigo ASCII do caracterer %c eh %d.\n",c,c);
+    return 0;
+}
